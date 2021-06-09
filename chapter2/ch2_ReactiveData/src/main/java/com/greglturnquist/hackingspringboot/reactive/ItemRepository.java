3b3a825afd1168bbd7d86a943f017ecccff11b46
bulhwi <mm225012@gmail.com>
2021-06-09 20:44:09 +0900

    주석 처리

--- chapter2/ch2_ReactiveData/src/main/java/com/greglturnquist/hackingspringboot/reactive/ItemRepository.java
+++ chapter2/ch2_ReactiveData/src/main/java/com/greglturnquist/hackingspringboot/reactive/ItemRepository.java
@@ -8,9 +8,9 @@ public interface ItemRepository extends ReactiveCrudRepository<Item, String> {
     // 입력한 검색어가 이름에 포함된 상품을 반환하는 메서드
     Flux<Item> findByNameContaining(String partialName);
 
-    @Query("{'name' : ?0, 'age' : ?1}")
-    Flux<Item> findItemsForCustomerMonthlyReport(String name, int age);
-
-    @Query(sort = "{'age' : -1}")
-    Flux<Item> findSortedStuffForWeeklyReport();
+//    @Query("{'name' : ?0, 'age' : ?1}")
+//    Flux<Item> findItemsForCustomerMonthlyReport(String name, int age);
+//
+//    @Query(sort = "{'age' : -1}")
+//    Flux<Item> findSortedStuffForWeeklyReport();
 }
